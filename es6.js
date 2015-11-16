7a38c5a14e96dc6eb253d623b7d51341a7759c87
Brandon Mills <mills.brandont@gmail.com>
2015-11-16 02:58:08 -0500

    Initial commit

---
+++ es6.js
@@ -0,0 +1,33 @@
+module.exports = {
+	"root": true,
+
+	"extends": "./es5",
+
+	"ecmaFeatures": {
+		"modules": true
+	},
+
+	"env": {
+		"es6": true
+	},
+
+	"rules": {
+		"no-class-assign": 2,
+		"no-const-assign": 2,
+		"no-dupe-class-members": 2,
+		"no-iterator": 0,
+		"no-this-before-super": 2,
+
+		"arrow-parens": 0,
+		"arrow-spacing": 2,
+		"computed-property-spacing": [0, "never"],
+		"constructor-super": 2,
+		"generator-star-spacing": 2,
+		"object-shorthand": 2,
+		"prefer-arrow-callback": 2,
+		"prefer-const": 2,
+		"prefer-spread": 2,
+		"prefer-reflect": 2,
+		"prefer-template": 2
+	}
+};
---
+++ react.js
@@ -0,0 +1,46 @@
+module.exports = {
+	"ecmaFeatures": {
+		"jsx": true,
+		"experimentalObjectRestSpread": true
+	},
+
+	"plugins": [
+		"react"
+	],
+
+	"env": {
+		"es6": true
+	},
+
+	"rules": {
+		"jsx-quotes": [2, "prefer-double"],
+
+		"react/display-name": 2,
+		"react/forbid-prop-types": 0,
+		"react/jsx-boolean-value": 2,
+		"react/jsx-closing-bracket-location": 2,
+		"react/jsx-curly-spacing": 2,
+		"react/jsx-indent-props": [2, "tab"],
+		"react/jsx-max-props-per-line": 0,
+		"react/jsx-no-duplicate-props": 2,
+		"react/jsx-no-literals": 0,
+		"react/jsx-no-undef": 2,
+		"react/jsx-sort-prop-types": 2,
+		"react/jsx-sort-props": 0,
+		"react/jsx-uses-react": 2,
+		"react/jsx-uses-vars": 2,
+		"react/no-danger": 2,
+		"react/no-did-mount-set-state": 2,
+		"react/no-did-update-set-state": 2,
+		"react/no-direct-mutation-state": 2,
+		"react/no-multi-comp": 2,
+		"react/no-set-state": 0,
+		"react/no-unknown-property": 2,
+		"react/prop-types": 1,
+		"react/react-in-jsx-scope": 2,
+		"react/require-extension": 2,
+		"react/self-closing-comp": 2,
+		"react/sort-comp": 2,
+		"react/wrap-multilines": 2
+	}
+};
