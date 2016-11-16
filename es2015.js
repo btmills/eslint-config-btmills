cac0902fd0f5c9fef255b6d1751877f181417d2c
Brandon Mills <mills.brandont@gmail.com>
2016-11-16 15:57:59 -0500

    Upgrade import plugin to v2.2.0

--- es2015.js
+++ es2015.js
@@ -49,27 +49,34 @@ module.exports = {
 		"symbol-description": "error",
 		"template-curly-spacing": "error",
 
+		"import/no-absolute-path": "error",
 		"import/no-amd": "error",
 		"import/no-commonjs": "error",
 		"import/no-deprecated": "warn",
 		"import/no-duplicates": "error",
+		"import/no-dynamic-require": "error",
 		"import/no-extraneous-dependencies": "error",
+		"import/no-internal-modules": "off",
 		"import/no-mutable-exports": "error",
 		"import/no-named-as-default": "error",
 		"import/no-named-as-default-member": "error",
+		"import/no-named-default": "error",
 		"import/no-namespace": "off",
 		"import/no-nodejs-modules": "off",
 		"import/no-restricted-paths": "off",
+		"import/no-unassigned-imports": "off",
 		"import/no-unresolved": "error",
+		"import/no-webpack-loader-syntax": "error",
 
 		"import/default": "error",
 		"import/export": "error",
 		"import/extensions": "off",
-		"import/imports-first": "error",
+		"import/first": "error",
 		"import/named": "error",
 		"import/namespace": "error",
 		"import/newline-after-import": "error",
 		"import/order": "off",
-		"import/prefer-default-export": "warn"
+		"import/prefer-default-export": "warn",
+		"import/unambiguous": "off"
 	}
 };
